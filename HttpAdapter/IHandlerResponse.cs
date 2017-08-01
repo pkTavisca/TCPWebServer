ccbe16323072341d88fabf2047249c993fc9ece2
Prabhash <pmohanty@tavisca.com>
2017-08-01 16:54:08 +0530

    Added IHandlerResponse interface

---
+++ HttpAdapter/IHandlerResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpAdapter
+{
+    interface IHandlerResponse
+    {
+        string Status { get; set; }
+        string Content { get; set; } //TODO...define a list
+        uint Length { get; set; }
+        string EntityBody { get; set; } 
+    }
+}
